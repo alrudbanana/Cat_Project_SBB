4292832e6052d1644f5f1c13f418ab55c25136b2
migyeong <pratt3343@gmail.com>
2023-03-06 23:15:38 +0900

    [2023.03.06] 프론트단 연결

--- src/main/java/com/shop/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.entity.Product;
-
-public interface ItemRepository extends JpaRepository<Product, Long> {
-
-}
