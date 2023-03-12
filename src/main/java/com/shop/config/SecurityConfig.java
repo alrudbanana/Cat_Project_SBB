3a7e2fa228d90feba76010180b23f4d235d8c703
migyeong <pratt3343@gmail.com>
2023-03-12 17:03:22 +0900

    Revert "[2023.03.12]item 안됨"
    This reverts commit 11da03ac4b0b9bfce4e64615e3c2c8ecaeceaf72.

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,52 +1,59 @@
 package com.shop.config;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.shop.member.service.MemberService;
+
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+		
+		.and()
+			.csrf()
+			.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+		.and()
+			.headers()
+			.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+		.and()
+			.formLogin()
+			.loginPage("/member/login")
+			.defaultSuccessUrl("/")
+		.and()
+			.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+			.logoutSuccessUrl("/")
+			.invalidateHttpSession(true)//로그아웃 이후 세션 삭제 여부
+			;
+	return http.build();
+	}
+    
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	}
+	 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/member/login")
-                .defaultSuccessUrl("/")
-                .usernameParameter("email")
-                .failureUrl("/member/login/error")
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                .logoutSuccessUrl("/")
-        ;
+	AuthenticationManager authenticateionManager(AuthenticationConfiguration authenticationConfiguration) 
+	throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
-        http.authorizeHttpRequests()
-                .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
-                .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-        ;
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-        ;
-
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+   
 
 }
--- src/main/java/com/shop/item/ItemSellStatus.java
+++
@@ -1,5 +0,0 @@
-package com.shop.item;
-
-public enum ItemSellStatus {
-    SELL, SOLD_OUT
-}
